bd9195e9ddb9706eb8416f7059ae16c63c48247c
Mensur <mensurdurakovic5@gmail.com>
2021-01-25 07:12:18 +0100

    added streams

--- 1-nodejs/index.js
+++ 1-nodejs/index.js
@@ -1,1 +1,5 @@
-console.log("****", global);
+console.time("loop");
+for (var i = 0; i < 1000000; i += 1) {
+  // Do nothing
+}
+console.timeEnd("loop");
--- 5-http/index.js
+++ 5-http/index.js
@@ -1,1 +1,21 @@
-console.log("****", global);
+const https = require("https");
+
+const options = {
+  hostname: "jsonplaceholder.typicode.com",
+  path: "/users",
+  method: "GET",
+};
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on("data", (d) => {
+    process.stdout.write(d);
+  });
+});
+
+req.on("error", (error) => {
+  console.error(error);
+});
+
+req.end();
